2bed328c75f76cd65cd49a7ad46b0893c9b96482
Champion176 <69591661+Champion176@users.noreply.github.com>
2021-05-21 09:05:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var formbg
+var form,
+function preload()
+{  
+	formbg=loadImage ("js/download.jpg") 
+   
+}
+
+function setup() {
+	createCanvas(1200, 700);
+form=new Form()
+
+
+//prompt("hi")
+
+
+}
+
+
+function draw() {
+ 
+
+
+ form.display()
+}
+
+
+
